7cdef85888626c700f981710aa7b334749381eae
aphil311 <41242437+aphil311@users.noreply.github.com>
2021-09-08 16:19:14 -0400

    improved fade-in/type effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
 
+// var i = 0;
+// var txt = "Greetings and salutations.";
+// var speed = 75;
+
+// function typeWriter() {
+//     if (i < txt.length) {
+//         document.getElementById("title-text").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+$(document).ready(function(){
     var i = 0;
     var txt = "Greetings and salutations.";
     var speed = 75;
+    setTimeout(typeWriter, 75);
 
     function typeWriter() {
         if (i < txt.length) {
-            document.getElementById("title-text").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
+        document.getElementById("title-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
         }
     }
 
+    $("#subtitle").append("I'm Aidan Phillips, a first-year computer science student at Princeton University.");
+    $("#subtitle").hide().delay(2500).fadeIn(1000);
+});
 
-    
