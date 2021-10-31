c1786c378a5dbf068d2d621b70749006a39dbddd
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2021-10-31 17:54:31 -0400

    cleaned up some code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-
-// var i = 0;
-// var txt = "Greetings and salutations.";
-// var speed = 75;
-
-// function typeWriter() {
-//     if (i < txt.length) {
-//         document.getElementById("title-text").innerHTML += txt.charAt(i);
-//         i++;
-//         setTimeout(typeWriter, speed);
-//     }
-// }
-
 $(document).ready(function(){
     var i = 0;
     var txt = "Greetings and salutations.";
